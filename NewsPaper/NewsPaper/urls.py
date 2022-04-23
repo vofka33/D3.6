58977959dbbf892c5651784ce5ecc854683313d9
Roman K <qdrome@yandex.ru>
2022-04-23 16:22:45 +0300

    Итоговое задание D5.6 недогрузившиеся файлы

--- NewsPaper/NewsPaper/urls.py
+++ NewsPaper/NewsPaper/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from news.views import PostsList
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('pages/', include('django.contrib.flatpages.urls')),
    path('news/', include('news.urls')),
+   path('', PostsList.as_view())
 ]
